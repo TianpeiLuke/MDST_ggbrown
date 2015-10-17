20e7975e58a3e2c58518d7e94110f9cddc314fd3
Tianpei <tianpei@umich.edu>
2015-10-17 07:55:49 -0400

    model construction via xgboost

--- Springleaf_model_construction.py
+++ Springleaf_model_construction.py
@@ -1,25 +1,72 @@
+# coding: utf-8
+
+#Python script for model construction via xgboost
 #import Springleaf_data_preprocessing
 
 import xgboost as xgb
-import scipy.sparse
-import pickle
+import numpy as np
+import pandas as pd
+from numpy import nan as NA
+from sklearn.cross_validation import train_test_split
 
-
-nline_trn = 145232
-nline_tst = 145233
+nline_trn = 145232-1
+nline_tst = 145233-1
 nBatch = 20000
-rlist = range(np.ceil(float(nline_trn)/float(nBatch)).astype(int)) 
 Y = pd.read_csv('./data/train_label.csv')
 y_train = Y.values # read training labels
-for i in rlist:
-    #read training data
-    if i == 0:
-        train = pd.read_csv("./data/train_cleaned.csv", nrows=nBatch)
-    else: 
-        train = pd.read_csv("./data/train_cleaned.csv", nrows=nBatch,
-                            skiprows= range(1,i*nBatch+1))
-   
-   
+print('Loading train and test data from csv file...')
+Xtrain = pd.read_csv("./data/train_cleaned.csv")
+Xtest = pd.read_csv("./data/test_cleaned.csv")
+#Xtrain.drop(['ID'], axis= 1, inplace = True)
+ID = Xtest['ID']
+Xtest.drop(['ID'], axis = 1, inplace = True)
+#    else: 
+#        train = pd.read_csv("./data/train_cleaned.csv", nrows=nBatch,
+#                            skiprows= range(1,i*nBatch+1))
+
+# split data
+print('Splitting data ...')
+Xtrain_c, Xeval, Ytrain_c, Yeval = train_test_split(Xtrain.values, y_train, test_size = 0.3)
+
+print('Loading train and test data into sparse matrix...')
+# load it into xgboost sparse matrix
+dtrain = xgb.DMatrix(data = Xtrain_c, missing = NA, label = Ytrain_c)
+deval = xgb.DMatrix(data = Xeval, missing = NA, label = Yeval)
 
 
+dtest = xgb.DMatrix(data = Xtest.values, missing= NA)  
 
+print('Specifying parameters ...')
+# specify parameters via map, definition are same as c++ version
+param = {'max_depth': 11,
+          'eta': 0.02, 
+          'subsample': 0.7,
+          'colsample_bytree': 0.8,
+          'silent': 0, 
+          'eval_metric': 'auc',
+          'alpha': 0.0005,
+          'lambda' : 1,
+          'objective':'binary:logistic' }
+
+# specify validations set to watch performance
+watchlist  = [(deval,'eval'), (dtrain,'train')]
+num_round = 950 #0.790154  #646 0.789907
+print('Training ..')
+bst = xgb.train(param, dtrain, num_round, watchlist)
+
+# this is prediction
+print('Prediction ...')
+preds = bst.predict(dtest)
+#print ('error=%f' % ( sum(1 for i in range(len(preds)) if int(preds[i]>0.5)!=labels[i]) /float(len(preds))))
+
+# save model
+bst.save_model('./model/xgb.model')
+# dump model
+bst.dump_model('./model/dump.raw.txt')
+ 
+ # save dmatrix into binary buffer
+dtest.save_binary('./model/dtest.buffer')
+
+submission = pd.DataFrame(data = preds, index = ID, columns = ['target'] )
+print("Save to submission file")
+submission.to_csv('./data/submission.csv')
